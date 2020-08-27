9a8e883fafe87ff0991a00af14031d40d80b12c8
JanaKhanji <JanaKhanji@users.noreply.github.com>
2020-08-27 12:07:35 +0300

    first editing

--- lib/main.dart
+++ lib/main.dart
@@ -9,34 +9,29 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.teal[900],
+          title: const Text('Pass APP'),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.add),
+              tooltip: 'add',
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: const Icon(Icons.menu),
+              tooltip: 'menu',
+              onPressed: () {},
+            ),
+          ],
+        ),
         backgroundColor: Colors.teal,
-        body: SafeArea(
+        body:
+
+        SafeArea(
             child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          //mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            CircleAvatar(
-              radius: 50.0,
-              backgroundImage: AssetImage('images/angela.jpg'),
-            ),
-            Text(
-              'Angela Yu',
-              style: TextStyle(
-                fontFamily: 'Pacifico',
-                fontSize: 40.0,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              'FLUTTER DEVELOPER',
-              style: TextStyle(
-                fontFamily: 'Source Sans Pro',
-                color: Colors.teal.shade100,
-                fontSize: 20.0,
-                letterSpacing: 2.5,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
             SizedBox(
               height: 20.0,
               width: 150.0,
@@ -47,34 +42,19 @@ class MyApp extends StatelessWidget {
             Card(
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
                 child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    color: Colors.teal,
-                  ),
                   title: Text(
-                    '+44 123 456 789',
+                    'facebook',
                     style: TextStyle(
                       color: Colors.teal.shade900,
                       fontFamily: 'Source Sans Pro',
                       fontSize: 20.0,
                     ),
                   ),
+                    subtitle: Text(
+                      'username:'
+                          'pass:'
+                    ),
                 )),
-            Card(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.email,
-                    color: Colors.teal,
-                  ),
-                  title: Text(
-                    'angela@email.com',
-                    style: TextStyle(
-                        fontSize: 20.0,
-                        color: Colors.teal.shade900,
-                        fontFamily: 'Source Sans Pro'),
-                  ),
-                ))
           ],
         )),
       ),
